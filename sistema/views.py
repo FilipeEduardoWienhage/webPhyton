cfb427802feabb482639d96376c3b96f41cdfc1d
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2024-10-11 21:15:45 -0300

    commit

--- sistema/views.py
+++ sistema/views.py
@@ -26,10 +26,50 @@ def calculadora(request: HttpRequest) -> HttpResponse:
 
 
 def calcular(request: HttpRequest) -> HttpResponse:
+    # request.GET é o metodo da requisão, quais são possiveis de utilizar:
+    # request.GET a informação vai na URL
+    # request.POST a informação vai por de baixo dos panos
+    # .get => é utilizado para obter um valor
     numero1 = int(request.GET.get("numero1"))
     numero2 = int(request.GET.get("numero2"))
     soma = numero1 + numero2
+    if numero1 > numero2:
+        maior = "Primeiro Número"
+    else:
+        maior = "Segundo Número"
+
     dados_para_html = {
-        "soma": soma
+        "soma": soma,
+        "maior": maior,
+        "numero1": numero1,
+        "numero2": numero2,
     }
     return render(request, "resultado.html", context=dados_para_html)
+
+
+def aluno(request: HttpRequest) -> HttpResponse:
+    return render(request, "aluno.html", context={})
+
+
+def media(request: HttpRequest) -> HttpResponse:
+    nota1 = int(request.GET.get("nota1"))
+    nota2 = int(request.GET.get("nota2"))
+    nota3 = int(request.GET.get("nota3"))
+    media = (nota1 + nota2 + nota3)/3
+
+    if media >= 7:
+        situacao = "Aprovado"
+    elif media >=5 and media < 7:
+        situacao = "Em Exame"
+    else:
+        situacao = "Reprovado"
+
+    dados_aluno = {
+        "nota1": nota1,
+        "nota2": nota2,
+        "nota3": nota3,
+        "media": media,
+        "status": situacao        
+    }
+
+    return render(request, "calcular-media.html", context=dados_aluno)
